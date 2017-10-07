2f640ee0d68be0ff7273a3f1d628f47ded8eb5bf
Alex Apetrei <alexaapetrei@gmail.com>
2017-10-07 12:41:54 +0300

    adjusted for azure

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World of Azure!')
 })
 
-app.listen(3000, function () {
+var port = process.env.PORT || 1337;
+
+app.listen(port, function () {
   console.log('Example app listening on port 3000!')
 })
